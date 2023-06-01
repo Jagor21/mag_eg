1caf65d1ca2fde83a359301e868c0e22b8d81c3d
jagorkushnir <jagor.kushnir@gmail.com>
2023-06-01 15:27:22 +0400

    init commit

---
+++ app/src/main/java/com/eternal_joy/magic_egypt/MagicEgyptApp.kt
@@ -0,0 +1,95 @@
+package com.eternal_joy.magic_egypt
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.appsflyer.AppsFlyerConversionListener
+import com.appsflyer.AppsFlyerLib
+import com.appsflyer.AppsFlyerProperties
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+private const val LOG_TAG = "MagicEgyptApp"
+
+class MagicEgyptApp : Application() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    val conversionData = MutableStateFlow<Any?>("")
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationsChannels()
+        createSilentNotificationChannel()
+
+        sharedPreferences = getSharedPreferences("your_prefs", MODE_PRIVATE)
+        val prefEditor = sharedPreferences.edit()
+        val conversionDataListener = object : AppsFlyerConversionListener {
+            override fun onConversionDataSuccess(data: MutableMap<String, Any>?) {
+                Log.d(LOG_TAG, data.toString())
+                var campaign = data?.get("campaign").toString()
+                if (campaign.isNotEmpty()) {
+                    prefEditor.putString("conversion_data", campaign).commit()
+                    conversionData.update { campaign }
+                } else {
+                    campaign = data?.get("adset").toString()
+                    prefEditor.putString("conversion_data", data?.get("campaign").toString()).commit()
+                    conversionData.update { data?.get("campaign") }
+                }
+            }
+
+            override fun onConversionDataFail(error: String?) {
+            }
+
+            override fun onAppOpenAttribution(data: MutableMap<String, String>?) {
+            }
+
+            override fun onAttributionFailure(error: String?) {
+            }
+        }
+        AppsFlyerLib.getInstance().setDebugLog(true)
+        AppsFlyerLib.getInstance()
+            .init(getString(R.string.appsflyer_dev_key), conversionDataListener, this)
+        AppsFlyerLib.getInstance().start(this)
+        val id = AppsFlyerLib.getInstance().getAppsFlyerUID(this)
+        id?.let { Log.d(LOG_TAG, "AppsFlyerUID: $it") }
+        val cuid = AppsFlyerProperties.getInstance().getString(AppsFlyerProperties.APP_USER_ID)
+        Log.d(LOG_TAG, "CUID: $cuid")
+    }
+
+    private fun createNotificationsChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                getString(R.string.reminders_notification_channel_id),
+                getString(R.string.reminders_notification_channel_name),
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            ContextCompat.getSystemService(this, NotificationManager::class.java)
+                ?.createNotificationChannel(channel)
+        }
+    }
+
+    private fun createSilentNotificationChannel() {
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (manager.getNotificationChannel(MagicEgyptFCMService.SILENT_CHANNEL_ID) == null) {
+                val channel = NotificationChannel(
+                    MagicEgyptFCMService.SILENT_CHANNEL_ID,
+                    "WheelOfFortune silent events",
+                    NotificationManager.IMPORTANCE_LOW
+                )
+                channel.enableLights(false)
+                channel.enableVibration(false)
+                channel.setSound(null, null)
+                manager.createNotificationChannel(channel)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/eternal_joy/magic_egypt/MagicEgyptFCMService.kt
@@ -0,0 +1,74 @@
+package com.eternal_joy.magic_egypt
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MagicEgyptFCMService: FirebaseMessagingService() {
+
+
+    companion object {
+        const val CHANNEL_ID = "magic_egypt_channel_id"
+        const val SILENT_CHANNEL_ID = "magic_egypt_silent_channel_id"
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("onNewToken", token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        try {
+            val notification = message.notification
+            if(notification != null){
+                val title = notification.title ?: ""
+                val text = notification.body ?: ""
+                showFCMNotification(title = title, text = text)
+            }
+        } catch (e: Exception) {
+            Log.d("MagicEgyptFCMService", e.message.toString())
+        }
+    }
+
+    private fun showFCMNotification(title: String, text: String) {
+        val intent = Intent(baseContext, MainActivity::class.java)
+        val channelId = "notification_channel"
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.putExtra("is_from_notification", true)
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.getActivity(
+                baseContext, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_ONE_SHOT
+            )
+        } else {
+            PendingIntent.getActivity(
+                baseContext, 0, intent, PendingIntent.FLAG_ONE_SHOT
+            )
+        }
+        val notification = NotificationCompat.Builder(baseContext, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setSmallIcon(R.drawable.ic_app_logo)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Firebase notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notification.build())
+    }
+}
